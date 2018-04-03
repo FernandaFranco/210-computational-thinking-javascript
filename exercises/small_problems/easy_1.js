7094fd3dc4ba1d59452bf9f16b9b2fbd0ef365ef
Fernanda Franco <francodias.fernanda@gmail.com>
2018-04-02 20:55:26 -0400

    more small problems

--- exercises/small_problems/easy_1.js
+++ exercises/small_problems/easy_1.js
@@ -1,3 +1,35 @@
+// var i;
+// var lowerLimit;
+// var upperLimit;
+// var number;
+
+// lowerLimit = Number(prompt('Specify the lower limit: (inclusive)'));
+// upperLimit = Number(prompt('Specify the upper limit: (inclusive)'));
+// number = lowerLimit;
+// if (lowerLimit % 2 === 1) number += 1;
+
+// do {
+//   console.log(number);
+//   number += 2;
+// } while (number <= upperLimit);
+
+// var areaOther;
+// var typeOther;
+// var SQMETERS_TO_SQFEET = 10.7639;
+// var inputType = prompt('Meters or feet?');
+// var length = Number(prompt('Enter the length of the room in ' + inputType + ':'));
+// var width = Number(prompt('Enter the width of the room in ' + inputType + ':'));
+// var area = length * width;
+// if (inputType === 'meters') {
+//   areaOther = area * SQMETERS_TO_SQFEET;
+//   typeOther = 'feet';
+// } else if (inputType === 'feet') {
+//   areaOther = area / SQMETERS_TO_SQFEET;
+//   typeOther = 'meters';
+// }
+
+// console.log('The area of the room is ' + area.toFixed(2) + ' square ' + inputType + ' (' + areaOther.toFixed(2) + ' square ' + typeOther + ').');
+
 // var billAmount = Number(prompt('What is the bill?'));
 // var tipPercentage = Number(prompt('What is the tip percentage?'));
 
@@ -38,21 +70,87 @@
 
 // console.log('The ' + operation + ' of the integers between ' + array[0] + ' and ' + array[array.length - 1] + ' is ' + result.toFixed(0) + '.');
 
-function shortLongShort(string1, string2) {
-  var long;
-  var short;
+// function shortLongShort(string1, string2) {
+//   var long;
+//   var short;
 
-  if (string1.length > string2.length) {
-    long = string1;
-    short = string2;
-  } else {
-    long = string2;
-    short = string1;
+//   if (string1.length > string2.length) {
+//     long = string1;
+//     short = string2;
+//   } else {
+//     long = string2;
+//     short = string1;
+//   }
+
+//   console.log(short + long + short);
+// }
+
+// shortLongShort('abc', 'defgh');    // "abcdefghabc"
+// shortLongShort('abcde', 'fgh');    // "fghabcdefgh"
+// shortLongShort('', 'xyz');         // "xyz"
+
+// function isLeapYear(year) {
+//   if (year < 1752) {
+//     return (year % 4 === 0);
+//   }
+
+//   if (year % 4 === 0) {
+//     if (year % 100 === 0 ) {
+//       if (year % 400 === 0) {
+//         return true
+//       }
+//       return false
+//     }
+//     return true
+//   }
+
+//   return false
+// }
+
+// console.log(isLeapYear(2016) == true);
+// console.log(isLeapYear(2015) == false);
+// console.log(isLeapYear(2100) == false);
+// console.log(isLeapYear(2400) == true);
+// console.log(isLeapYear(240000) == true);
+// console.log(isLeapYear(240001) == false);
+// console.log(isLeapYear(2000) == true);
+// console.log(isLeapYear(1900) == false);
+// console.log(isLeapYear(1752) == true);
+// console.log(isLeapYear(1700) == true);
+// console.log(isLeapYear(1) == false);
+// console.log(isLeapYear(100) == true);
+// console.log(isLeapYear(400) == true);
+
+// function multisum(number) {
+//   var i;
+//   var sum = 0;
+
+//   for (i = 1; i <= number; i++){
+//     if (i % 3 === 0 || i % 5 === 0) {
+//       sum += i;
+//     }
+//   }
+
+//   return sum;
+// }
+
+// console.log(multisum(3));       // 3
+// console.log(multisum(5));       // 8
+// console.log(multisum(10));      // 33
+// console.log(multisum(1000));    // 234168
+
+function asciiValue(string) {
+  var i;
+  var sum = 0;
+
+  for (i = 0; i < string.length; i++) {
+    sum += string.charCodeAt(i);
   }
 
-  console.log(short + long + short);
+  return sum;
 }
 
-shortLongShort('abc', 'defgh');    // "abcdefghabc"
-shortLongShort('abcde', 'fgh');    // "fghabcdefgh"
-shortLongShort('', 'xyz');         // "xyz"
+console.log(asciiValue('Four score'));         // 984
+console.log(asciiValue('Launch School'));      // 1251
+console.log(asciiValue('a'));                  // 97
+console.log(asciiValue(''));                   // 0
---
+++ exercises/small_problems/easy_2.js
@@ -0,0 +1,145 @@
+// function crunch(string) {
+//   var i;
+//   var newString = '';
+
+//   for (i = 0; i < string.length; i++) {
+//     if (string[i] !== string[i-1]) {
+//       newString += string[i];
+//     }
+//   }
+
+//   return newString;
+// }
+// console.log(crunch('ddaaiillyy ddoouubbllee'));    // "daily double"
+// console.log(crunch('4444abcabccba'));              // "4abcabcba"
+// console.log(crunch('ggggggggggggggg'));            // "g"
+// console.log(crunch('a'));                          // "a"
+// console.log(crunch(''));                           // ""
+
+
+// function logInBox(string, maxWidth) {
+//   var length = string.length;
+//   if (length > maxWidth) {
+//     length = maxWidth;
+//     string = string.substr(0, maxWidth);
+//   }
+//   console.log('+-' + '-'.repeat(length) + '-+');
+//   console.log('| ' + ' '.repeat(length) + ' |');
+//   console.log('| ' + string + ' |');
+//   console.log('| ' + ' '.repeat(length) + ' |');
+//   console.log('+-' + '-'.repeat(length) + '-+');
+// }
+
+// logInBox('To boldly go where no one has gone before.', 10);
+// logInBox('');
+
+// function stringy(integer) {
+//   var string = '1';
+
+//   for (var i = 1; i < integer; i++) {
+//     if (string[i - 1] === '1') {
+//       string += '0';
+//     } else {
+//       string += '1';
+//     }
+//   }
+
+//   return string;
+// }
+
+// console.log(stringy(6));    // "101010"
+// console.log(stringy(9));    // "101010101"
+// console.log(stringy(4));    // "1010"
+// console.log(stringy(7));    // "1010101"
+
+// function findFibonacciIndexByLength(digits) {
+//   var fibonacci = 1;
+//   var index = 2;
+//   var prevFibonacci = 1;
+//   var temp;
+
+//   while (String(fibonacci).length < digits) {
+//     temp = fibonacci;
+//     fibonacci += prevFibonacci;
+//     prevFibonacci = temp;
+//     index += 1;
+//   }
+//   return index;
+// }
+
+// console.log(findFibonacciIndexByLength(2));       // 7
+// console.log(findFibonacciIndexByLength(10));      // 45
+// console.log(findFibonacciIndexByLength(16));      // 74
+
+// function triangle(n) {
+//   var stars = '';
+//   var spaces;
+
+//   for (var i = 1; i <= n; i++) {
+//     stars += '*';
+//     spaces = ' '.repeat(n - i);
+//     console.log(spaces + stars);
+//   }
+// }
+
+// triangle(5);
+// triangle(9);
+
+// var noun = prompt('Enter a noun:');
+// var verb = prompt('Enter a verb:');
+// var adjective = prompt('Enter an adjective:');
+// var adverb = prompt('Enter an adverb:');
+
+// console.log('Do you ' + verb + ' your ' + adjective + ' ' +
+//             noun + ' ' + adverb + '? That\'s hilarious!')
+
+
+// function twice(number) {
+//   if (isDoubleNumber(number)) {
+//     return number;
+//   }
+
+//   return number * 2;
+// }
+
+// function isDoubleNumber(number) {
+//   var string = String(number);
+//   var center = string.length / 2;
+
+//   return (string.substr(0, center) === string.substr(center));
+// }
+
+// console.log(twice(37));          // 74
+// console.log(twice(44));          // 44
+// console.log(twice(334433));      // 668866
+// console.log(twice(444));         // 888
+// console.log(twice(107));         // 214
+// console.log(twice(103103));      // 103103
+// console.log(twice(3333));        // 3333
+// console.log(twice(7676));        // 7676
+
+// function getGrade(score1, score2, score3) {
+//   var averageScore = (score1 + score2 + score3) / 3;
+
+//   if (averageScore >= 90) {
+//     return 'A';
+//   } else if (averageScore >= 80) {
+//     return 'B';
+//   } else if (averageScore >= 70) {
+//     return 'C';
+//   } else if (averageScore >= 60) {
+//     return 'D';
+//   } else {
+//     return 'F';
+//   }
+// }
+
+// console.log(getGrade(95, 90, 93));    // "A"
+// console.log(getGrade(50, 50, 95));    // "D"
+
+function cleanUp(string) {
+  var newString = string.replace(/[^a-zA-Z]+/g, ' ');
+  return newString;
+}
+
+console.log(cleanUp("---what's my +*& line?"));
