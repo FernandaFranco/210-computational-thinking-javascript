904fed442ba9a1aa630d3f7f56cb9075c2d7e5ea
Fernanda Franco <francodias.fernanda@gmail.com>
2018-05-01 20:00:00 -0400

    small problems easy 3 + updated notes

--- exercises/small_problems/easy_3.js
+++ exercises/small_problems/easy_3.js
@@ -25,22 +25,118 @@ var included =  function(element) {
   return element === number;
 }
 
-number = prompt('Enter the 1st number:');
+// number = prompt('Enter the 1st number:');
 numbers.push(number);
-number = prompt('Enter the 2nd number:');
+// number = prompt('Enter the 2nd number:');
 numbers.push(number);
-number = prompt('Enter the 3rd number:');
+// number = prompt('Enter the 3rd number:');
 numbers.push(number);
-number = prompt('Enter the 4th number:');
+// number = prompt('Enter the 4th number:');
 numbers.push(number);
-number = prompt('Enter the 5th number:');
+// number = prompt('Enter the 5th number:');
 numbers.push(number);
-number = prompt('Enter the last number:');
+// number = prompt('Enter the last number:');
 
-sentence = 'The number ' + number + ' does not appear in [' + String(numbers) + '].'
+sentence = 'The number ' + number + ' does not appear in [' + numbers.join(', ') + '].'
 
 if (numbers.some(included)) {
-  sentence = 'The number ' + number + ' appears in [' + String(numbers) + '].'
+  sentence = 'The number ' + number + ' appears in [' + numbers.join(', ') + '].'
 }
 
-console.log(sentence);
+// console.log(sentence);
+
+var retirementYear;
+var workingYears;
+
+// var age = parseInt(prompt('What is your age?'), 10);
+// var retirementAge = parseInt(prompt('At what age would you like to retire?'), 10);
+// var currentYear = new Date().getFullYear();
+// workingYears = retirementAge - age;
+// retirementYear = currentYear + workingYears;
+
+
+// console.log("It's " + currentYear + ". You will retire in " + retirementYear + ".");
+// console.log("You have only " + workingYears + " years of work to go!");
+
+function isPalindrome(string) {
+  var reverseString = string.split('').reverse().join('');
+  return reverseString === string;
+}
+
+// console.log(isPalindrome('madam'));               // true
+// console.log(isPalindrome('Madam'));               // false (case matters)
+// console.log(isPalindrome("madam i'm adam"));      // false (all characters matter)
+// console.log(isPalindrome('356653'));              // true
+
+function isRealPalindrome(string) {
+  var charsArray = string.toLowerCase().match(/[\w]+/g);
+  var strippedString = charsArray.join('');
+  console.log(strippedString);
+  return isPalindrome(strippedString);
+}
+
+// console.log(isRealPalindrome('madam'));               // true
+// console.log(isRealPalindrome('Madam'));               // true (case does not matter)
+// console.log(isRealPalindrome("Madam, I'm Adam"));     // true (only alphanumerics matter)
+// console.log(isRealPalindrome('356653'));              // true
+// console.log(isRealPalindrome('356a653'));             // true
+// console.log(isRealPalindrome('123ab321'));            // false
+
+function isPalindromicNumber(number) {
+  return isPalindrome(String(number));
+}
+
+// console.log(isPalindromicNumber(34543));        // true
+// console.log(isPalindromicNumber(123210));       // false
+// console.log(isPalindromicNumber(22));           // true
+// console.log(isPalindromicNumber(5));            // true
+
+function runningTotal(numbers) {
+  var total = 0;
+  // var runningTotals = [];
+
+  // for (var i = 0; i < numbers.length; i += 1) {
+  //   total += numbers[i];
+  //   runningTotals.push(total);
+  // }
+
+  var runningTotals = numbers.map(function(element) {
+    total += element;
+    return total;
+  })
+
+  return runningTotals;
+}
+
+// console.log(runningTotal([2, 5, 13]));             // [2, 7, 20]
+// console.log(runningTotal([14, 11, 7, 15, 20]));    // [14, 25, 32, 47, 67]
+// console.log(runningTotal([3]));                    // [3]
+// console.log(runningTotal([]));                     // []
+
+function swap(string) {
+  var words = string.split(' ');
+  // var swappedWords =[];
+  // var i;
+
+  // for (i = 0; i < words.length; i+= 1) {
+  //   var chars = words[i].split('');
+  //   var temp = chars[0];
+  //   chars[0] = chars[chars.length - 1];
+  //   chars[chars.length - 1] = temp;
+  //   swappedWords[i] = chars.join('');
+  // }
+
+  swappedWords = words.map( function(word) {
+    var chars = word.split('');
+    var temp = chars[0];
+    chars[0] = chars[chars.length - 1];
+    chars[chars.length - 1] = temp;
+    RETURN chars.join('');
+  });
+
+  return swappedWords.join(' ');
+}
+
+console.log(swap('Oh what a wonderful day it is'));  // "hO thaw a londerfuw yad ti si"
+console.log(swap('Abcde'));                          // "ebcdA"
+console.log(swap('a'));                              // "a"
--- notes.md
+++ notes.md
@@ -8,6 +8,10 @@ Be able to explain clearly the following topics:
 
 - primitive values, types and type conversions
 
+PRIMITIVE VALUES
+
+there are five of them. They are the most basic values one can think of including undefined, null, boolean, string, and numbers. Primitive values are passed by value
+
 - variable scopes and hoisting
 
 - function declarations, expressions and scopes
@@ -17,6 +21,14 @@ Be able to explain clearly the following topics:
 arrays are implemented with objects internally. arrays have a length property, which objects do not.
 
 - assignments and comparison
+
+VARIABLE INITIALIZATION AND ASSIGNMENT
+
+A variable initialization and assignment at the same line doesn't have a return value (returns undefined). An assignment (or reassignment) returns the value assigned to the variable.
+
+STRINGS COMPARISON
+
+Recall that strings are compared based on standard lexicographical ordering, using Unicode values.
 
 - pure functions and side effects
 
